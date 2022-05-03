fc65f6925112579eddb8877ba7d9fd8bbb7609e7
Nayara Goulart <nayara.goulart@axiadigitalsolutions>
2022-05-02 21:44:07 -0300

    Stage 1 - Completed *-*

---
+++ web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+        colors: {
+            brand:{
+                500: '#8257e6' // Quantas cores quiser para usar no Tailwind
+            }
+        }
+    },
+  },
+  plugins: [],
+}
